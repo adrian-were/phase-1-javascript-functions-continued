6742ee08845110fd9b9d6181eebcaf524fcda077
Adrian <adrianshisia@gmail.com>
2023-06-19 12:47:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate") {
+ return `This Saturday, I want to ${fun}!`
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+
+const mondayWork = function (today = "go to the office") {       
+ return `This Monday, I will ${today}.`
+}
+mondayWork()
+mondayWork("work from home")
+
+function wrapAdjective(okay = "special") {
+ return `You are ${okay}`
+}
